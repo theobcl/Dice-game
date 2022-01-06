8cb91cb206d0083ab6a68cab1240e036d57e9727
Alex Vigreux <alex.vigreux@kedgebs.com>
2022-01-06 19:42:29 +0100

    Validations first_name, last_name and nick_name added

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :rooms
   has_many :games, through: :rooms
+
+  validates :first_name, uniqueness: { scope: :last_name }
+  validates :nick_name, uniqueness: true, length: { minimum: 2 }
 end
