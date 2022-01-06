0ddb00b356b5f095d5cc06343de34d14d2741e53
Alex Vigreux <alex.vigreux@kedgebs.com>
2022-01-06 19:45:49 +0100

    Avatar added to user's model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :rooms
   has_many :games, through: :rooms
+  has_one_attached :avatar
 
   validates :first_name, uniqueness: { scope: :last_name }
   validates :nick_name, uniqueness: true, length: { minimum: 2 }
