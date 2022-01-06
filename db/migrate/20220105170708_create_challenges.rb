8c7aae19dcec6af90ec2e3b93e449c1edd543406
Aymeric Payen <aymeric.payen@gmail.com>
2022-01-06 11:34:29 +0100

    migration is fixed

--- db/migrate/20220105170708_create_challenges.rb
+++ db/migrate/20220105170708_create_challenges.rb
@@ -2,7 +2,7 @@ class CreateChallenges < ActiveRecord::Migration[6.1]
   def change
     create_table :challenges do |t|
       t.references :game, null: false, foreign_key: true
-      t.references :player, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
       t.references :gag, null: false, foreign_key: true
 
       t.timestamps
