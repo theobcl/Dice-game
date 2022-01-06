4fa334535f50f1e5c18167e1eb41160b7d326cf1
Alex Vigreux <alex.vigreux@kedgebs.com>
2022-01-06 20:20:29 +0100

    Game's validations added

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,4 +2,7 @@ class Game < ApplicationRecord
   belongs_to :room
   has_many :game_attendees
   has_many :results
+
+  validates :player_count, length: { maximum: 10 }
+  validates :game_name, uniqueness: true
 end
